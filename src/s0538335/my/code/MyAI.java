6f06964c5d3c442c372c6ef74cde535ff4fcfa98
FeeB <s0538335@htw-berlin.de>
2014-05-10 22:44:52 +0200

    refactoring & fix error from seek

--- src/s0538335/my/code/MyAI.java
+++ src/s0538335/my/code/MyAI.java
@@ -3,26 +3,30 @@ package s0538335.my.code;
 import java.awt.Polygon;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+
 import org.lwjgl.opengl.GL11;
+
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.Info;
 import s0538335.my.code.Tile;
 
 public class MyAI extends AI {
 
+	private static final int TILE_SIZE = 20;
+
 	private float acceleration;
 
 	private int rotationTime = 2;
 	private int wishedTime = 4;
-	
-	private static final int SIZE = 10;
-	private boolean[][] raster = new boolean[info.getWorld().getWidth() / SIZE][info.getWorld().getHeight() / SIZE];
-	
-	ArrayList<Tile> openList = new ArrayList<Tile>();
-	ArrayList<Tile> closeList = new ArrayList<Tile>();
-	ArrayList<Tile> path = new ArrayList<Tile>();
-	
-	private int counter;
+
+	private boolean[][] raster = new boolean[info.getWorld().getWidth() / TILE_SIZE][info.getWorld().getHeight() / TILE_SIZE];
+
+	private ArrayList<Tile> openList = new ArrayList<Tile>();
+	private ArrayList<Tile> closeList = new ArrayList<Tile>();
+	private ArrayList<Tile> path = new ArrayList<Tile>();
+
+	private int currentTileIndex;
+	private Tile currentTile;
 
 	public MyAI(Info arg0) {
 		super(arg0);
@@ -38,14 +42,11 @@ public class MyAI extends AI {
 
 	@Override
 	public float getAngularAcceleration() {
-		createRaster();
-		findPath();
 		return seek();
 	}
 
 	@Override
 	public String getName() {
-
 		return "Autooo";
 	}
 
@@ -53,203 +54,198 @@ public class MyAI extends AI {
 
 	@Override
 	public void drawDebugStuff() {
-		
+
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE, currentTile.getyCoord() * TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE + TILE_SIZE, currentTile.getyCoord() * TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE + TILE_SIZE, currentTile.getyCoord() * TILE_SIZE + TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE, currentTile.getyCoord() * TILE_SIZE + TILE_SIZE, 0.1f);
+		GL11.glEnd();
+
+		GL11.glBegin(GL11.GL_LINE);
+		GL11.glVertex2f(info.getX(), info.getY());
+		GL11.glVertex2f(currentTile.getxCoord() * TILE_SIZE, currentTile.getxCoord() * TILE_SIZE);
+		// System.out.println(counter);
+		// System.out.println(path.get(counter-1).getxCoord() * SIZE);
+		// System.out.println(path.get(counter-1).getxCoord() * SIZE);
+		GL11.glEnd();
+
 	}
 
 	public void createRaster() {
-		
+
 		Polygon[] obstacle = info.getWorld().getObstacles();
 
-		for (int x = 0; x < info.getWorld().getWidth() / SIZE; x++) {
-			for (int y = 0; y < info.getWorld().getHeight() / SIZE; y++) {
+		for (int x = 0; x < info.getWorld().getWidth() / TILE_SIZE; x++) {
+			for (int y = 0; y < info.getWorld().getHeight() / TILE_SIZE; y++) {
 				raster[x][y] = true;
-				Rectangle2D.Float rec = new Rectangle2D.Float(x * SIZE, y * SIZE, SIZE, SIZE);
+				Rectangle2D.Float rec = new Rectangle2D.Float(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 				for (Polygon obstaclePoint : obstacle) {
 					if (obstaclePoint.intersects(rec)) {
 						raster[x][y] = false;
 					}
 
 				}
-//				GL11.glBegin(GL11.GL_QUADS);
-//			    GL11.glVertex3f(x*SIZE, y*SIZE,0.1f);
-//			    GL11.glVertex3f(x*SIZE + SIZE, y*SIZE,0.1f);
-//			    GL11.glVertex3f(x*SIZE + SIZE, y*SIZE + SIZE,0.1f);
-//			    GL11.glVertex3f(x*SIZE, y*SIZE + SIZE,0.1f);
-//			    GL11.glEnd();
+				// GL11.glBegin(GL11.GL_QUADS);
+				// GL11.glVertex3f(x*SIZE, y*SIZE,0.1f);
+				// GL11.glVertex3f(x*SIZE + SIZE, y*SIZE,0.1f);
+				// GL11.glVertex3f(x*SIZE + SIZE, y*SIZE + SIZE,0.1f);
+				// GL11.glVertex3f(x*SIZE, y*SIZE + SIZE,0.1f);
+				// GL11.glEnd();
 			}
-
 		}
-
 	}
-	
-	public void findPath(){		
-		Tile startTile = new Tile();
-		startTile.setxCoord(Math.round(info.getX()/SIZE));
-		startTile.setyCoord(Math.round(info.getY()/SIZE));
+
+	public void findPath() {
+		Tile startTile = new Tile(TILE_SIZE, matchRaster(Math.round(info.getX() / TILE_SIZE)), matchRaster(Math.round(info.getY() / TILE_SIZE)));
 		startTile.setAccessible(raster[startTile.getxCoord()][startTile.getyCoord()]);
-		startTile.setWeight(0);
-		setRightXYCoord(startTile);
-		
-		Tile targetTile = new Tile();
-		targetTile.setxCoord(info.getCurrentCheckpoint().x/SIZE);
-		targetTile.setyCoord(info.getCurrentCheckpoint().y/SIZE);
-		System.out.println(targetTile.getyCoord());
+
+		Tile targetTile = new Tile(TILE_SIZE, matchRaster(info.getCurrentCheckpoint().x / TILE_SIZE), matchRaster(info.getCurrentCheckpoint().y / TILE_SIZE));
 		targetTile.setAccessible(raster[targetTile.getxCoord()][targetTile.getyCoord()]);
-		setRightXYCoord(targetTile);
-		System.out.println(targetTile.getyCoord());
-		
-		//first add startTile to openList
+
+		// first add startTile to openList
+		closeList.clear();
+		openList.clear();
 		openList.add(startTile);
-		
-		
-		while(!openList.isEmpty()){
-		
+
+		while (!openList.isEmpty()) {
+
 			Tile actualTile = findLowestWeight();
 			openList.remove(actualTile);
 			closeList.add(actualTile);
-			
-			if(actualTile.isTheSame(targetTile)){
+
+			if (actualTile.isTheSame(targetTile)) {
 				targetTile.setPrev(actualTile.getPrev());
 				break;
 			}
-			
+
 			ArrayList<Tile> neighbours = findNeighbour(actualTile, targetTile);
-			 
+
 			for (Tile tile : neighbours) {
-				if (tile.accessible && !listContainsTile(closeList, tile)){
-					if(!listContainsTile(openList, tile)){
-						openList.add(tile);
+				if (tile.getAccessible() && !listContainsTile(closeList, tile)) {
+					if (!listContainsTile(openList, tile)) {
 						tile.setPrev(actualTile);
-					}else{
-						if(getEqualTile(openList, tile).getWeight() > tile.getWeight()){
-							getEqualTile(openList, tile).setWeight(tile.getWeight());
-							getEqualTile(openList, tile).setPrev(actualTile);
-							getEqualTile(openList, tile).setTotalWeight();
-						}
+						openList.add(tile);
+					} else if (getEqualTile(openList, tile).getWeight() > tile.getWeight()) {
+						getEqualTile(openList, tile).setWeight(tile.getWeight());
+						getEqualTile(openList, tile).setPrev(actualTile);
+						getEqualTile(openList, tile).setTotalWeight();
 					}
-					
 				}
 			}
 		}
 		storePath(startTile, targetTile);
 	}
-	
-	public void storePath(Tile startTile, Tile targetTile){
-		Tile actualTile = new Tile();
-		actualTile.getObject(targetTile);
-				
-		while (!actualTile.isTheSame(startTile)){
+
+	public void storePath(Tile startTile, Tile targetTile) {
+		Tile actualTile = targetTile.clone();
+
+		while (!actualTile.isTheSame(startTile)) {
 			path.add(actualTile);
 			actualTile = actualTile.getPrev();
 		}
-		counter = path.size();
+		currentTileIndex = 0;
+		currentTile = path.get(currentTileIndex);
 	}
-	
-	private ArrayList<Tile> findNeighbour(Tile actualTile, Tile targetTile){
+
+	private ArrayList<Tile> findNeighbour(Tile actualTile, Tile targetTile) {
 		ArrayList<Tile> tiles = new ArrayList<>();
-		
+
 		for (int row = -1; row < 2; row++) {
 			for (int col = -1; col < 2; col++) {
-				if (row == 0 && col == 0 || actualTile.getxCoord() + row * SIZE < 0 || actualTile.getyCoord() + col * SIZE < 0
-						|| actualTile.getxCoord() + row * SIZE >= info.getWorld().getWidth() / SIZE || actualTile.getyCoord() + col * SIZE >= info.getWorld().getHeight() / SIZE){
+				if ((row == 0 && col == 0)
+						|| actualTile.getxCoord() + row * TILE_SIZE < 0
+						|| actualTile.getyCoord() + col * TILE_SIZE < 0
+						|| actualTile.getxCoord() + row * TILE_SIZE >= info.getWorld().getWidth() / TILE_SIZE 
+						|| actualTile.getyCoord() + col * TILE_SIZE >= info.getWorld().getHeight() / TILE_SIZE) {
 					continue;
-				}else if (!raster[actualTile.getxCoord() + row * SIZE][actualTile.getyCoord() + col * SIZE]){
+				} else if (!raster[actualTile.getxCoord() + row * TILE_SIZE][actualTile.getyCoord() + col * TILE_SIZE]) {
 					continue;
-				}else{
-					Tile tile = new Tile();
-					tile.setxCoord(actualTile.getxCoord() + row * SIZE);
-					tile.setyCoord(actualTile.getyCoord() + col * SIZE);
-					tile.setAccessible(raster[actualTile.getxCoord() + row * SIZE][actualTile.getyCoord() + col * SIZE]);
-					
+				} else {
+					Tile tile = new Tile(TILE_SIZE, actualTile.getxCoord() + row * TILE_SIZE, actualTile.getyCoord() + col * TILE_SIZE);
+					tile.setAccessible(raster[actualTile.getxCoord() + row * TILE_SIZE][actualTile.getyCoord() + col * TILE_SIZE]);
+
 					int dist = (int) Math.sqrt(Math.pow(targetTile.getxCoord() - tile.getxCoord(), 2) + Math.pow(targetTile.getyCoord() - tile.getyCoord(), 2));
 					tile.setHeuristicWeight(dist);
-					
-					if(Math.abs(row)==1 && Math.abs(col)==1){
+
+					if (Math.abs(row) == 1 && Math.abs(col) == 1) {
 						tile.setWeight(14);
-					}else{
+					} else {
 						tile.setWeight(10);
 					}
-					
+
 					tile.setTotalWeight();
-					
+
 					tiles.add(tile);
 				}
 			}
 		}
 		return tiles;
-		
+
 	}
-	
-	public Tile findLowestWeight(){
+
+	public Tile findLowestWeight() {
 		int lowestWeight = Integer.MAX_VALUE;
-		Tile tileWithLowestWeight = new Tile();
+		Tile tileWithLowestWeight = null;
 		for (Tile tile : openList) {
-			if (tile.getTotalWeight() < lowestWeight){
+			if (tile.getTotalWeight() < lowestWeight) {
 				lowestWeight = tile.getTotalWeight();
 				tileWithLowestWeight = tile;
 			}
 		}
 		return tileWithLowestWeight;
 	}
-	
-	public boolean listContainsTile(ArrayList<Tile> list, Tile tile){
+
+	public boolean listContainsTile(ArrayList<Tile> list, Tile tile) {
 		for (Tile existingTile : list) {
-			if(tile.getxCoord() == existingTile.getxCoord() && tile.getyCoord() == existingTile.getyCoord()) {
+			if (tile.getxCoord() == existingTile.getxCoord() && tile.getyCoord() == existingTile.getyCoord()) {
 				return true;
 			}
 		}
-		return false;	
+		return false;
 	}
-	
-	public Tile getEqualTile(ArrayList<Tile> list, Tile tile){
+
+	public Tile getEqualTile(ArrayList<Tile> list, Tile tile) {
 		for (Tile existingTile : list) {
-			if(tile.getxCoord() == existingTile.getxCoord() && tile.getyCoord() == existingTile.getyCoord()) {
+			if (tile.getxCoord() == existingTile.getxCoord() && tile.getyCoord() == existingTile.getyCoord()) {
 				return existingTile;
 			}
 		}
 		return null;
 	}
-	
-	public void setRightXYCoord(Tile tile){
-		int x = (tile.getxCoord()/10) * 10;
-		int y = (tile.getyCoord()/10) * 10;
-		
-		tile.setxCoord(x);
-		tile.setyCoord(y);
+
+	private int matchRaster(int value) {
+		return (value / TILE_SIZE) * TILE_SIZE;
 	}
-	
-	
+
 	// seek to target
 
 	public float seek() {
 
-//		acceleration = info.getMaxAcceleration();
-//		int actualX = (int) (((info.getX() / SIZE) / 100) * 10);
-//		int actualY = (int) (((info.getY() / SIZE) / 100) * 10);
-//		int targetX = (int) (((info.getCurrentCheckpoint().x / SIZE) / 10) * 10);
-//		int targetY = (int) (((info.getCurrentCheckpoint().y / SIZE) / 10) * 10);
-//		Rectangle2D.Float recActualPos = new Rectangle2D.Float(actualX, actualY, SIZE, SIZE);
-//		Rectangle2D.Float recTarget = new Rectangle2D.Float(targetX, targetY, SIZE, SIZE);
-//		
-//		
-//		
-//		while(!recActualPos.intersects(recTarget)){
-		
-		
-		float newOrientation = getNewOrientation(path.get(counter).getxCoord(), path.get(counter).getxCoord());
+		float newOrientation = getNewOrientation(currentTile.getxCoord() * TILE_SIZE, currentTile.getxCoord() * TILE_SIZE);
 
 		float difference = newOrientation - info.getOrientation();
 
-		float wunschdrehGeschwindigkeite = difference * info.getMaxAngularVelocity() / rotationTime;
-		float drehBeschleunigung = (wunschdrehGeschwindigkeite - info.getAngularVelocity()) / wishedTime;
+		float preferedRotarySpeed = difference * info.getMaxAngularVelocity() / rotationTime;
+		float rotaryAcceleration = (preferedRotarySpeed - info.getAngularVelocity()) / wishedTime;
 
-		counter--;
-		if(counter < 0){
+		if (currentTile.contains(info.getX(), info.getY()) && currentTile.contains(info.getCurrentCheckpoint().getX(), info.getCurrentCheckpoint().getY())) {
 			findPath();
+		} else if (currentTile.contains(info.getX(), info.getY())) {
+			currentTileIndex++;
+			currentTile = path.get(currentTileIndex);
 		}
 		
-		return drehBeschleunigung;
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE, currentTile.getyCoord() * TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE + TILE_SIZE, currentTile.getyCoord() * TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE + TILE_SIZE, currentTile.getyCoord() * TILE_SIZE + TILE_SIZE, 0.1f);
+		GL11.glVertex3f(currentTile.getxCoord() * TILE_SIZE, currentTile.getyCoord() * TILE_SIZE + TILE_SIZE, 0.1f);
+		GL11.glEnd();
 		
+		return rotaryAcceleration;
+		
+
 	}
 
 	// Orientation to the target
--- src/s0538335/my/code/Tile.java
+++ src/s0538335/my/code/Tile.java
@@ -1,80 +1,92 @@
 package s0538335.my.code;
 
+import java.awt.geom.Rectangle2D;
+
 public class Tile {
-		int xCoord;
-		int yCoord;
-		Tile prev;
-		int weight;
-		int heuristicWeight;
-		int totalWeight;
-		boolean accessible;
-		
-		
-		
-		public int getxCoord() {
-			return xCoord;
+
+	private int size;
+	private int x;
+	private int y;
+	private Tile prev;
+	private int weight;
+	private int heuristicWeight;
+	private int totalWeight;
+	private boolean accessible;
+	private Rectangle2D.Float asRectangle;
+
+	public Tile(int size, int x, int y) {
+		this.size = size;
+		this.x = x;
+		this.y = y;
+		this.asRectangle = new Rectangle2D.Float(x * size, y * size, size, size);
+	}
+	
+	public int getxCoord() {
+		return x;
+	}
+
+	public int getyCoord() {
+		return y;
+	}
+
+	public Tile getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Tile prev) {
+		this.prev = prev;
+	}
+
+	public boolean getAccessible() {
+		return accessible;
+	}
+
+	public void setAccessible(boolean accessible) {
+		this.accessible = accessible;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getHeuristicWeight() {
+		return this.heuristicWeight;
+	}
+
+	public void setHeuristicWeight(int heuristicWeight) {
+		this.heuristicWeight = heuristicWeight;
+	}
+
+	public int getTotalWeight() {
+		return totalWeight;
+	}
+
+	public void setTotalWeight() {
+		this.totalWeight = this.weight + this.heuristicWeight;
+	}
+
+	public Tile clone() {
+		Tile tile = new Tile(size, x, y);
+		tile.setPrev(prev);
+		tile.setAccessible(accessible);
+		tile.setHeuristicWeight(heuristicWeight);
+		tile.setWeight(weight);
+		return tile;
+	}
+
+	public boolean isTheSame(Tile tile) {
+		if (this.x == tile.getxCoord() && this.y == tile.y) {
+			return true;
 		}
-		public void setxCoord(int xCoord) {
-			this.xCoord = xCoord;
-		}
-		
-		public int getyCoord() {
-			return yCoord;
-		}
-		public void setyCoord(int yCoord) {
-			this.yCoord = yCoord;
-		}
-		
-		public Tile getPrev() {
-			return prev;
-		}
-		public void setPrev(Tile prev) {
-			this.prev = prev;
-		}
-		
-		
-		public boolean getAccessible(){
-			return accessible;
-		}
-		public void setAccessible(boolean accessible){
-			this.accessible = accessible;
-		}
-		
-		public int getWeight() {
-			return weight;
-		}
-		public void setWeight(int weight) {
-			this.weight = weight;
-		}
-		
-		public int getHeuristicWeight(){
-			return heuristicWeight;
-		}
-		public void setHeuristicWeight(int heuristicWeight) {
-			this.heuristicWeight = heuristicWeight;
-		}
-		
-		public int getTotalWeight(){
-			return totalWeight;
-		}
-		public void setTotalWeight(){
-			this.totalWeight = this.weight + this.heuristicWeight;
-		}
-		
-		public void getObject(Tile tile){
-			this.xCoord = tile.getxCoord();
-			this.yCoord = tile.getyCoord();
-			this.accessible = tile.getAccessible();
-			this.heuristicWeight = tile.getHeuristicWeight();
-			this.weight = tile.getWeight();
-			this.totalWeight = tile.getTotalWeight();
-			this.prev = tile.getPrev();
-		}
-		
-		public boolean isTheSame(Tile tile){
-			if (this.xCoord == tile.getxCoord() && this.yCoord == tile.yCoord){
-				return true;
-			}
-			return false;
-		}
+		return false;
+	}
+	
+	public boolean contains(double x, double y) {
+		return asRectangle.contains(x, y);
+	}
+	
 }
